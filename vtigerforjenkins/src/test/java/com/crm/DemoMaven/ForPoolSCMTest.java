0cefe7ac3cebe25d2ad0ac0beaad94ba43c89721
Lenovo <Lenovo@Bibhudatta>
2022-03-28 14:56:39 +0530

    Poll scm check push

---
+++ vtigerforjenkins/src/test/java/com/crm/DemoMaven/ForPoolSCMTest.java
@@ -0,0 +1,10 @@
+package com.crm.DemoMaven;
+
+import org.testng.annotations.Test;
+
+public class ForPoolSCMTest {
+	@Test
+	public void classTest() {
+		System.err.println("This is for poll scm check ");
+	}
+}
